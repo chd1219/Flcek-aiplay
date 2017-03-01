722736c1380c1215ecd31789639fcbe857ee39b2
chd <chd12190@163.com>
2017-03-01 23:13:27 +0800

    20170301

---
+++ Comm.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using System.Diagnostics;
+using System.Xml;
+using System.Net;
+
+namespace Fleck.aiplay
+{
+    class Setting
+    {
+        static public string port { get; set; }
+        static public string level { get; set; }
+        static public bool isSupportCloudApi { get; set; }
+        static public string engine { get; set; }
+    }
+
+    class Log
+    {
+        private string LogPath;
+        private StreamWriter log;
+        public Log()
+        {
+            LogPath = DateTime.Now.ToLongDateString();
+            log = new StreamWriter(LogPath + Setting.port + ".log", true);
+        }
+        public void WriteInfo(string message)
+        {
+            WriteInfo("{0}", message);
+        }
+        public void WriteInfo(string format, params object[] obj)
+        {
+            try
+            {
+                log.WriteLine(string.Format("[{0}] {1}", System.DateTime.Now, string.Format(format, obj)));
+                log.Flush();
+            }
+            catch
+            {
+                // Nothing to do
+            }
+        }
+    }
+
+    class Msg
+    {
+        public IWebSocketConnection connection { get; set; }
+        public string message { get; set; }
+        public bool isreturn { get; set; }
+        public Msg()
+        {
+            connection = null;
+            message = "";
+            isreturn = false;
+        }
+    }
+
+    class Comm
+    {
+        StreamWriter PipeWriter { get; set; }
+        Msg currentmsg { get; set; }
+        Log log;
+        Queue MsgQueue;
+
+        public void WriteInfo(string message)
+        {
+            log.WriteInfo(message);
+        }
+
+        public void ReceiveMessage()
+        {
+            executeCommand(Setting.engine, "");
+            return;
+        }
+
+        public string QuerybestFromCloud(string board)
+        {
+            if (!Setting.isSupportCloudApi)
+            {
+                return null;
+            }
+            string serverUrl = "http://api.chessdb.cn:81/chessdb.php?action=querybest&board=" + board;
+            string postData = "";
+            string serverResult = "";
+            serverResult = HttpPostConnectToServer(serverUrl, postData);
+            return serverResult;
+        }
+
+        public string QueryallFromCloud(string message)
+        {
+            string board = message.Substring(9, message.Length - 9);
+            if (!Setting.isSupportCloudApi)
+            {
+                return null;
+            }
+            string serverUrl = "http://api.chessdb.cn:81/chessdb.php?action=queryall&board=" + board;
+            string postData = "";
+            string serverResult = "";
+            serverResult = HttpPostConnectToServer(serverUrl, postData);
+            serverResult = serverResult.Replace("move:", "");//替换为空
+            serverResult = serverResult.Replace("score:", "");//替换为空
+            serverResult = serverResult.Replace("rank:", "");//替换为空
+            serverResult = serverResult.Replace("note:", "");//替换为空
+            return "Queryall" + serverResult;
+        }
+
+        public void DealMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (MsgQueue.Count > 0)
+                    {
+                        Msg msg = new Msg();
+                        msg = (Msg)MsgQueue.Dequeue();
+                        currentmsg = msg;
+                        string board = msg.message.Substring(13, msg.message.Length - 9 - 12);
+                        string serverResult = QuerybestFromCloud(board);
+
+                        PipeWriter.Write(msg.message + "\r\n");
+                        string move = "";
+                       
+                        if (serverResult != null && serverResult.IndexOf("move:") != -1)
+                        {
+                            //Console.WriteLine(serverResult);
+                            if (board.Length > 61)
+                            {
+                                move = " bookmove " + serverResult.Substring(5, 4);
+                            }
+                            else if (board.Length < 51)
+                            {
+                                move = " egtbmove " + serverResult.Substring(5, 4);
+                            }
+                            //Console.WriteLine(move);
+                            log.WriteInfo(move);
+                        }
+                        PipeWriter.Write("go depth " + Setting.level + move + "\r\n");
+                        while (!msg.isreturn)
+                        {
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        void executeCommand(string strFile, string args)
+        {
+            Process p = new System.Diagnostics.Process();
+            p.StartInfo = new System.Diagnostics.ProcessStartInfo();
+            p.StartInfo.FileName = strFile;
+            p.StartInfo.Arguments = args;
+            p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            StreamReader reader = p.StandardOutput;//截取输出流
+            PipeWriter = p.StandardInput;//截取输入流
+
+            string line = reader.ReadLine();//每次读取一行
+            //Console.WriteLine(line);
+            while (true)
+            {
+                line = reader.ReadLine();
+                if (currentmsg != null && line != null)
+                {
+                    currentmsg.connection.Send(line);
+                    if (line.IndexOf("bestmove") != -1)
+                    {                        
+                        log.WriteInfo(currentmsg.connection.ConnectionInfo.ClientIpAddress + ":" + currentmsg.connection.ConnectionInfo.ClientPort.ToString() + " " + line);
+                        currentmsg.connection = null;
+                        currentmsg.isreturn = true;
+                    }
+
+                }
+                //Console.WriteLine(line);
+            }
+            //p.WaitForExit();
+        }
+
+        public void Start()
+        {
+            LoadXml();
+            log = new Log();
+            MsgQueue = new Queue();
+
+            OnReceive();
+            OnDeal();
+
+        }
+
+        public void OnReceive()
+        {
+            Thread receiveDataThread = new Thread(new ThreadStart(ReceiveMessage));
+            receiveDataThread.IsBackground = true;
+            receiveDataThread.Start();
+        }
+
+        public void OnDeal() 
+        {
+            Thread DealMeassgaehread = new Thread(new ThreadStart(DealMessage));
+            DealMeassgaehread.IsBackground = true;
+            DealMeassgaehread.Start();
+        }
+
+
+        public void OnMessage(Msg msg)
+        {
+            MsgQueue.Enqueue(msg);
+        }
+
+        public void LoadXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(".\\config.xml");
+            XmlNode xn = doc.SelectSingleNode("configuration");
+            XmlNodeList xnl = xn.ChildNodes;
+            foreach (XmlNode xn1 in xnl)
+            {
+                XmlElement xe = (XmlElement)xn1;
+                if (xe.GetAttribute("key").ToString() == "Port")
+                {
+                    Setting.port = xe.GetAttribute("value").ToString();
+                }
+                if (xe.GetAttribute("key").ToString() == "Level")
+                {
+                    Setting.level = xe.GetAttribute("value").ToString();
+                }
+                if (xe.GetAttribute("key").ToString() == "CloudAPI")
+                {
+                    Setting.isSupportCloudApi = Convert.ToBoolean(xe.GetAttribute("value"));
+                }
+                if (xe.GetAttribute("key").ToString() == "EnginePath")
+                {
+                    Setting.engine = xe.GetAttribute("value").ToString();
+                }
+            }
+        }
+
+        public string HttpPostConnectToServer(string serverUrl, string postData)
+        {
+            var dataArray = Encoding.UTF8.GetBytes(postData);
+            //创建请求  
+            var request = (HttpWebRequest)HttpWebRequest.Create(serverUrl);
+            request.Method = "POST";
+            request.ContentLength = dataArray.Length;
+            //设置上传服务的数据格式  
+            request.ContentType = "application/x-www-form-urlencoded";
+            //请求的身份验证信息为默认  
+            request.Credentials = CredentialCache.DefaultCredentials;
+            //请求超时时间  
+            request.Timeout = 10000;
+            //创建输入流  
+            Stream dataStream;
+            //using (var dataStream = request.GetRequestStream())  
+            //{  
+            //    dataStream.Write(dataArray, 0, dataArray.Length);  
+            //    dataStream.Close();  
+            //}  
+            try
+            {
+                dataStream = request.GetRequestStream();
+            }
+            catch (Exception)
+            {
+                return null;//连接服务器失败  
+            }
+            //发送请求  
+            dataStream.Write(dataArray, 0, dataArray.Length);
+            dataStream.Close();
+            //读取返回消息  
+            string res = "";
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                res = reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                log.WriteInfo(ex.Message);
+                // mylog.WriteInfo("{\"error\":\"connectToServer\",\"error_description\":\"" + ex.Message + "\"}");//连接服务器失败  
+            }
+            return res;
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+
+namespace Fleck.aiplay
+{    
+    class Server
+    {
+        static void Main()
+        {
+            Comm comm = new Comm();
+            comm.Start();
+            
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://0.0.0.0:"+Setting.port);     
+            
+            server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                        {
+                            allSockets.Add(socket);
+                            Console.WriteLine(socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort.ToString() + " Connected!");
+                            comm.WriteInfo(socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort.ToString() + " Connected!");
+                        };
+                    socket.OnClose = () =>
+                        {
+                            Console.WriteLine(socket.ConnectionInfo.ClientIpAddress + ":"+ socket.ConnectionInfo.ClientPort.ToString() + " Close!");
+                            comm.WriteInfo(socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort.ToString() + " Close!");
+                            allSockets.Remove(socket);
+                        };
+                    socket.OnMessage = message =>
+                        {
+                            comm.WriteInfo(socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort.ToString() + " " + message);        
+
+                            if (message.IndexOf("queryall") != -1)
+                            {
+                                socket.Send(comm.QueryallFromCloud(message));
+                                return;
+                                //Console.WriteLine(comm.QueryallFromCloud(message));
+                            }
+
+                            if (message == "list")
+                            {
+                               // int no = 1;
+                               // allSockets.ToList().ForEach(
+                               //     s => socket.Send((no++) + ": " + s.ConnectionInfo.ClientIpAddress + ":" + s.ConnectionInfo.ClientPort.ToString())
+                               //     );
+                                socket.Send("There are " + allSockets.Count + " clients online.");
+                            }   
+
+                            //过滤命令
+                            if (message.IndexOf("position") == -1)
+                            {
+                                return;
+                            }
+  
+                            
+
+                            Msg msg = new Msg();
+                            msg.connection = socket;
+                            msg.message = message;
+
+                            comm.OnMessage(msg);
+
+                            return;
+                        };
+                });
+
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }  
+}
